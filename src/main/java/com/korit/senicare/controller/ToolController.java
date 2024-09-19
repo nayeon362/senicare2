3bdfb566104d54cea209ba0be18a02389539bdc1
devJiraynor <jiraynor@naver.com>
2024-09-19 16:51:41 +0900

    2024.09.19. 16:50

---
+++ src/main/java/com/korit/senicare/controller/ToolController.java
@@ -0,0 +1,31 @@
+package com.korit.senicare.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.korit.senicare.dto.request.tool.PostToolRequestDto;
+import com.korit.senicare.dto.response.ResponseDto;
+import com.korit.senicare.service.ToolSerivce;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/tool")
+@RequiredArgsConstructor
+public class ToolController {
+    
+    private final ToolSerivce toolSerivce;
+
+    @PostMapping(value={"", "/"})
+    public ResponseEntity<ResponseDto> postTool(
+        @RequestBody @Valid PostToolRequestDto requestBody
+    ) {
+        ResponseEntity<ResponseDto> response = toolSerivce.postTool(requestBody);
+        return response;
+    }
+
+}
