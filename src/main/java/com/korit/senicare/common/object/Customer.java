33b49c3ec9f7d752df8422e9135ec57773b9db65
devJiraynor <jiraynor@naver.com>
2024-09-25 10:51:08 +0900

    2024.09.25. 10:50

--- src/main/java/com/korit/senicare/common/object/Customer.java
+++ src/main/java/com/korit/senicare/common/object/Customer.java
@@ -1,5 +1,11 @@
 package com.korit.senicare.common.object;
 
+import java.util.List;
+
+import com.korit.senicare.repository.resultSet.GetCustomerResultSet;
+
+import java.util.ArrayList;
+
 import lombok.Getter;
 
 @Getter
@@ -10,4 +16,26 @@ public class Customer {
     private String location;
     private String chargerName;
     private String chargerId;
+
+    private Customer(GetCustomerResultSet resultSet) {
+        this.customerNumber = resultSet.getCustomerNumber();
+        this.name = resultSet.getName();
+        this.birth = resultSet.getBirth();
+        this.location = resultSet.getLocation();
+        this.chargerName = resultSet.getChargerName();
+        this.chargerId = resultSet.getChargerId();
+    }
+
+    public static List<Customer> getList(List<GetCustomerResultSet> resultSets) {
+
+        List<Customer> customers = new ArrayList<>();
+
+        for (GetCustomerResultSet resultSet: resultSets) {
+            Customer customer = new Customer(resultSet);
+            customers.add(customer);
+        }
+
+        return customers;
+
+    }
 }
